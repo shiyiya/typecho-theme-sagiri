6905e030f146a30104985a61067d9236a44bcb1d
shiyiya <i@runtua.cn>
2019-10-26 12:58:41 +0800

    update build config

---
+++ merge.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cp ./*.php ./temp
+cp ./*.md ./temp
+cp ./package.json ./temp
+cp ./*.png ./temp
+cp -f ./js/* ./temp/js/
+cp -f ./css/* ./temp/css/
+cp -f ./doc/* ./temp/doc/
+cp -f ./component/* ./temp/component/
+cp -f ./libray/* ./temp/libray/
+cp -f ./util/* ./temp/util/
