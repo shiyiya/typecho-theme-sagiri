7b19a6a95282a77a0c80f6ebc7057952033a2ba4
shiyiya <shiyiya.11@gmail.com>
2023-05-26 19:06:53 +0800

    year's timeline

--- page-archive.php
+++ page-archive.php
@@ -18,30 +18,22 @@
                 <?php
                 $this->widget('Widget_Contents_Post_Recent', 'pageSize=' . $stat->publishedPostsNum)->to($archives);
                 $year = 0;
-                $mon = 0;
-                $i = 0;
-                $j = 0;
                 $f = false;
                 $output = '';
                 while ($archives->next()) {
                     $year_tmp = date('Y', $archives->created);
-                    $mon_tmp = date('m', $archives->created);
-                    $y = $year;
-                    $m = $mon;
 
-                    if ($year != $year_tmp || $mon != $mon_tmp) {
+                    if ($year != $year_tmp) {
                         $year = $year_tmp;
-                        $mon = $mon_tmp;
                         $output .= $f ? "</div>" : $output;
                         $output .= '<h1 class="archive-timeline-title" itemprop="name headline">'
-                            . date('M Y', $archives->created) .
+                            . date('Y', $archives->created) .
                             '</h1><div class="archive-timeline-group">';
                         $f = true;
                     }
-
                     $output .= '
                 <li class="archive-post" itemtype="http://schema.org/BlogPosting">
-                    <span class="post-meta"> ' . date('M j', $archives->created) . '</span>
+                    <span class="post-meta"> ' . date('m/j', $archives->created) . '</span>
                     <h2 class="archive-post-title"><a href="' . $archives->permalink . '">' . $archives->title . '</a></h2>
                 </li>';
                 }
